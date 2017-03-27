d7883309f2864d4655f2171fdee9832936940270
Kyle Simpson <getify@gmail.com>
2017-03-27 11:20:38 -0500

    adding stable-timers for normalizing timer-sensitive testing

--- scripts/node-tests.js
+++ scripts/node-tests.js
@@ -4,6 +4,9 @@
 
 var path = require("path");
 
+var StableTimers = require( "stable-timers" );
+StableTimers.replaceGlobals();
+
 global.$DOM = require(path.join("..","src","index.js"));
 
 global.QUnit = require("qunitjs");
